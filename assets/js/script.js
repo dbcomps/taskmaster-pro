d6f0b0c21c409c527040523d618baf2d1dfa8960
David Barraza <dbcomps@mac.com>
2022-08-16 17:13:19 -0500

    added drag & drop capabilities with jQuery UI and touch

--- assets/js/script.js
+++ assets/js/script.js
@@ -142,6 +142,60 @@ $(".list-group").on("blur", "input[type='text']", function() {
 	$(this).replaceWith(taskSpan);
 });
 
+// make ul elements with list-group class sortable using jQuery UI
+$(".card .list-group").sortable({
+  connectWith: $(".card .list-group"),
+  scroll: false,
+  tolerance: "pointer",
+  helper: "clone",
+  activate: function(event) {
+  	console.log("activate", this);
+  },
+  deactivate: function(event) {
+  	console.log("deactivate", this);
+  },
+  over: function(event) {
+  	console.log("over", event.target);
+  },
+  out: function(event) {
+  	console.log("out", event.target);
+  },
+  update: function(event) {
+	// array to store the task data index
+		var tempArr = [];
+  
+  	// loop over current set of children in sortable list
+  	$(this).children().each(function(){
+  		var text = $(this)
+  			.find("p")
+  			.text()
+  			.trim();
+  			
+  		var date = $(this)
+  			.find("span")
+  			.text()
+  			.trim();
+  			
+  		// add task data to the temp array as an object
+  		tempArr.push({
+  			text: text,
+  			date: date
+  		});
+  	});
+  	
+  	// trim down list's ID to match object property
+  	var arrName = $(this)
+  		.attr("id")
+  		.replace("list-", "");
+  		
+  	// update array on tasks object and saveTasks
+  	tasks[arrName] = tempArr;
+  	saveTasks();
+  	
+  	console.log(tempArr);
+	}
+});
+
 // modal was triggered
 $("#task-form-modal").on("show.bs.modal", function() {
   // clear values
@@ -176,6 +230,23 @@ $("#task-form-modal .btn-primary").click(function() {
   }
 });
 
+
+$("#trash").droppable({
+	accept: ".card .list-group-item",
+	tolerance: "touch",
+	drop: function(event, ui) {
+		ui.draggable.remove();
+		console.log("drop");
+	},
+	over: function(even, ui) {
+		console.log("over");
+	},
+	out: function(event, ui) {
+		console.log("out");
+	}
+});
+
+
 // remove all tasks
 $("#remove-tasks").on("click", function() {
   for (var key in tasks) {
@@ -184,6 +255,8 @@ $("#remove-tasks").on("click", function() {
   }
   saveTasks();
 });
+
+
 
 // load tasks for the first time
 loadTasks();
