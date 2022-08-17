b86513a040433c6c4859cad96f43505466af4bad
David Barraza <dbcomps@mac.com>
2022-08-16 20:29:13 -0500

    changes based on snapshot

--- assets/js/script.js
+++ assets/js/script.js
@@ -45,6 +45,78 @@ var saveTasks = function() {
   localStorage.setItem("tasks", JSON.stringify(tasks));
 };
 
+// make ul elements with list-group class sortable using jQuery UI
+$(".card .list-group").sortable({
+  connectWith: $(".card .list-group"),
+  scroll: false,
+  tolerance: "pointer",
+  helper: "clone",
+  activate: function(event, ui) {
+  	console.log("activate", this);
+  },
+  deactivate: function(event, ui) {
+  	console.log("deactivate", this);
+  },
+  over: function(event) {
+  	console.log("over", event.target);
+  },
+  out: function(event) {
+  	console.log("out", event.target);
+  },
+  update: function(event) {
+	// array to store the task data index
+		var tempArr = [];
+  
+  	// loop over current set of children in sortable list
+  	$(this).children().each(function(){
+  		var text = $(this)
+  			.find("p")
+  			.text()
+  			.trim();
+  			
+  		var date = $(this)
+  			.find("span")
+  			.text()
+  			.trim();
+  			
+  		// add task data to the temp array as an object
+  		tempArr.push({
+  			text: text,
+  			date: date
+  		});
+  	});
+  	
+  	// trim down list's ID to match object property
+  	var arrName = $(this)
+  		.attr("id")
+  		.replace("list-", "");
+  		
+  	// update array on tasks object and saveTasks
+  	tasks[arrName] = tempArr;
+  	saveTasks();
+  	
+  	console.log(tempArr);
+	},
+	stop: function(even) {
+		$(this).removeClass("dropover");
+	}
+});
+
+// trash icon can be dropped onto
+$("#trash").droppable({
+	accept: ".card .list-group-item",
+	tolerance: "touch",
+	drop: function(event, ui) {
+		ui.draggable.remove();
+		console.log("drop");
+	},
+	over: function(even, ui) {
+		console.log("over");
+	},
+	out: function(event, ui) {
+		console.log("out");
+	}
+});
 
 // task text was clicked
 $(".list-group").on("click", "p", function() {
@@ -142,59 +214,7 @@ $(".list-group").on("blur", "input[type='text']", function() {
 	$(this).replaceWith(taskSpan);
 });
 
-// make ul elements with list-group class sortable using jQuery UI
-$(".card .list-group").sortable({
-  connectWith: $(".card .list-group"),
-  scroll: false,
-  tolerance: "pointer",
-  helper: "clone",
-  activate: function(event) {
-  	console.log("activate", this);
-  },
-  deactivate: function(event) {
-  	console.log("deactivate", this);
-  },
-  over: function(event) {
-  	console.log("over", event.target);
-  },
-  out: function(event) {
-  	console.log("out", event.target);
-  },
-  update: function(event) {
-	// array to store the task data index
-		var tempArr = [];
-  
-  	// loop over current set of children in sortable list
-  	$(this).children().each(function(){
-  		var text = $(this)
-  			.find("p")
-  			.text()
-  			.trim();
-  			
-  		var date = $(this)
-  			.find("span")
-  			.text()
-  			.trim();
-  			
-  		// add task data to the temp array as an object
-  		tempArr.push({
-  			text: text,
-  			date: date
-  		});
-  	});
-  	
-  	// trim down list's ID to match object property
-  	var arrName = $(this)
-  		.attr("id")
-  		.replace("list-", "");
-  		
-  	// update array on tasks object and saveTasks
-  	tasks[arrName] = tempArr;
-  	saveTasks();
-  	
-  	console.log(tempArr);
-	}
-});
+
 
 // modal was triggered
 $("#task-form-modal").on("show.bs.modal", function() {
@@ -230,21 +250,6 @@ $("#task-form-modal .btn-primary").click(function() {
   }
 });
 
-
-$("#trash").droppable({
-	accept: ".card .list-group-item",
-	tolerance: "touch",
-	drop: function(event, ui) {
-		ui.draggable.remove();
-		console.log("drop");
-	},
-	over: function(even, ui) {
-		console.log("over");
-	},
-	out: function(event, ui) {
-		console.log("out");
-	}
-});
 
 
 // remove all tasks
